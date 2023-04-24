ff163320d58aa15e3fda80e43b43af6cdfa69814
natalia <ntla.brs@gmail.com>
2023-04-24 14:03:31 -0300

    testando deploy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import * as Styled from './AboutStyle';
+import ImgPort from '../img/MinhaPort.png';
 
 export default class About extends Component {
   render() {
     return (
-      <div>About</div>
+      <Styled.AboutSection>
+
+        <Styled.ImgDiv>
+          <Styled.ImgPort src={ ImgPort } />
+        </Styled.ImgDiv>
+        <Styled.AboutDiv>
+          <p>
+            icrum isci quam commodi
+            possimus rerum laborum quae in nihil unde praesentium voluptod blanditiis.
+          </p>
+        </Styled.AboutDiv>
+
+      </Styled.AboutSection>
     );
   }
 }
+//
